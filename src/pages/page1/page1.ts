13d50b62b31a68ff3d4d2a9a771f107fd7129589
Christopher Hardt <christopher.hardt@mind-me.de>
2016-10-29 18:02:49 +0200

    Simple Contact selector

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {Contacts} from 'ionic-native';
+
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
 
+  public contact;
+
   constructor(public navCtrl: NavController) {
     
+  }
+
+  //call by button
+  addContact(){
+    Contacts.pickContact().then(
+        (contacts) => {
+          this.contact = contacts;
+        },
+        (err) => {
+          console.log(err);
+        }
+    );
   }
 
 }
